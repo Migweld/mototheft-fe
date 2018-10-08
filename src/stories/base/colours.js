e1d1ef2e08333bdbd55a67532b08696a1f1a2aa2
Benjamin Hawkyard <benjamin@gathercontent.com>
2018-10-08 19:04:54 +0200

    Adding storybook and create-react-app

---
+++ src/stories/base/colours.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+storiesOf('Components', module).add('Blank Slate', () => (
+  <div>
+    <StoryItem
+      title="BlankSlate Component"
+      description="BlankSlate with the default style"
+    >
+      <BlankSlate>
+        <h2 className="blank-slate__heading">This is a blank slate</h2>
+        <p>Use me to show that something is empty.</p>
+        <Button
+          types={['secondary']}
+          clickHandler={() => console.log('do a thing')}
+        >
+          Do something
+        </Button>
+      </BlankSlate>
+    </StoryItem>
+    <StoryItem
+      title="BlankSlate Component"
+      description="BlankSlate with the arrow style"
+    >
+      <BlankSlate
+        slateStyle="arrow"
+      >
+        <h2 className="blank-slate__heading">Hey do something on the left</h2>
+        <p>Then put it over here.</p>
+      </BlankSlate>
+    </StoryItem>
+  </div>
+));
---
+++ src/stories/components/Button.js
@@ -0,0 +1,7 @@
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+  ));
